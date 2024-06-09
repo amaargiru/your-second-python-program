ac8caa4527518d92e65b252a5d3827e81189cb02
Mihail Emelyanov <war4one@gmail.com>
2024-06-09 21:18:34 +0500

    Модернизировал базовые классы

--- Code/Core/game.py
+++ Code/Core/game.py
@@ -1,20 +1,61 @@
 class Tile:
     """Плитка"""
+
     # Плитка выделена в отдельный класс для возможности дальнейшего расширения. Например, в консольной игре к ней можно
     # добавить свойство "Цвет", в режиме обучения можно выделять одинаковые плитки, расположенные рядом и т. д.
-    _value: int = 0
-    """Номинал плитки"""
+
+    def __init__(self, value: int):
+        self.value: int = value  # Call value setter
+        """Номинал плитки"""
+
+    @property
+    def value(self) -> int:
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if value < 0:
+            raise ValueError("The value of the tile must be not less than zero.")
+        if value % 2 != 0:
+            raise ValueError("The value of the tile must be an even number.")
+
+        self._value = value
 
 
 class Score:
-    _value: int = 0
     """Текущий счёт"""
-    _diff: int = 0
-    """Скорость изменения счёта"""  # Может быть полезна при индикации счёта. Высокая скорость изменения -> удачный ход -> красный цвет
+
+    def __init__(self, value: int):
+        self._value: int = value
+        """Величина текущего счёт"""
+        self._diff: int = 0
+        """Скорость изменения счёта"""
+        # Может быть полезна при индикации счёта. Высокая скорость изменения -> удачный ход -> поздравление красным цветом
+
+    @property
+    def value(self) -> int:
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if value < 0:
+            raise ValueError("The value of the scored points must be not less than zero.")
+        if value % 2 != 0:
+            raise ValueError("The value of the scored points must be an even number.")
+        if value < self._value:
+            raise ValueError("The value of the scored points should not decrease.")
+
+        self._diff = value - self._value
+        self._value = value
+
+    @property
+    def diff(self) -> int:
+        return self._diff
 
 
 class Game:
-    _board: list[Tile] = []
-    """Игровое поле"""
-    _score = Score()
-    _is_game_over: bool = False
+    def __init__(self, rows: int, cols: int):
+        _board: list[list[Tile]] = [[Tile(0)] * rows for _ in range(cols)]
+        """Игровое поле"""
+        _score: Score = Score(0)
+        _is_game_over: bool = False
