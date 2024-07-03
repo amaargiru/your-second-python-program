842a9619f39a91e371c7799a2b71047b5f31daf5
Mihail Emelyanov <war4one@gmail.com>
2024-07-03 11:51:36 +0500

    Extend pretty print for 2D list of Tiles

--- Code/CLI/py2048.py
+++ Code/CLI/py2048.py
@@ -4,4 +4,4 @@ from terminal import Terminal
 game = Game(rows=3, cols=5)
 game.reset()
 
-# Terminal.pretty_print_2d_list(game.board)
+Terminal.pretty_print_2d_list(game.board)
--- Code/CLI/terminal.py
+++ Code/CLI/terminal.py
@@ -1,12 +1,14 @@
 from colorama import Fore, Style, init
 
+from Core.game import Tile
+
 init(autoreset=True)
 
 
 class Terminal:
     @staticmethod
-    def pretty_print_2d_list(board):
-        max_length = max(max(len(str(item)) for item in row) for row in board)
+    def pretty_print_2d_list(board: list[list[Tile]]):
+        max_length = max(max(len(str(item.value)) for item in row) for row in board)
         separator = '+'.join('-' * (max_length + 2) for _ in range(len(board[0])))
         separator = f"+{separator}+"
 
@@ -14,9 +16,9 @@ class Terminal:
         for row in board:
             row_str = "|"
             for item in row:
-                item_str = str(item).center(max_length)
-                if Terminal._is_power_of_two(item):
-                    item_str = Terminal._get_color_for_power_of_two(item) + item_str + Style.RESET_ALL
+                item_str = str(item.value).center(max_length)
+                if Terminal._is_power_of_two(item.value):
+                    item_str = Terminal._get_color_for_power_of_two(item.value) + item_str + Style.RESET_ALL
                 row_str += f" {item_str} |"
             print(row_str)
             print(separator)
@@ -37,12 +39,13 @@ class Terminal:
         return Style.RESET_ALL  # Default color if not a power of two
 
 
-# Example usage
-board = [
-    [0, 2, 4, 8],
-    [16, 32, 64, 128],
-    [256, 512, 1024, 2048],
-    [4096, 8192, 16384, 32768]
-]
+if __name__ == "__main__":
+    # Example usage
+    board: list[list[Tile]] = [
+        [Tile(0), Tile(2), Tile(4), Tile(8)],
+        [Tile(16), Tile(32), Tile(64), Tile(128)],
+        [Tile(256), Tile(512), Tile(1024), Tile(2048)],
+        [Tile(4096), Tile(8192), Tile(16384), Tile(32768)]
+    ]
 
-Terminal.pretty_print_2d_list(board)
+    Terminal.pretty_print_2d_list(board)
