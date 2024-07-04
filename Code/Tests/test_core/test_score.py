d2d5d48c4ee570a2b5e37d4c94e0b67c0c409870
Mihail Emelyanov <war4one@gmail.com>
2024-07-04 15:25:33 +0500

    Modify some tests

--- Code/Tests/test_core/test_score.py
+++ Code/Tests/test_core/test_score.py
@@ -3,44 +3,48 @@ import pytest
 from Core.game import Score
 
 
-class TestScore:
+def test_initial_value():
+    score = Score(10)
+    assert score.value == 10
+    assert score.diff == 10
 
-    def test_initial_value(self):
-        score = Score(10)
-        assert score.value == 10
-        assert score.diff == 10
 
-    def test_update_value(self):
-        score = Score(10)
-        score.value = 14
-        assert score.value == 14
-        assert score.diff == 4
+def test_update_value():
+    score = Score(10)
+    score.value = 14
+    assert score.value == 14
+    assert score.diff == 4
 
-    def test_set_negative_value(self):
-        score = Score(10)
-        with pytest.raises(ValueError, match="The value of the scored points must be not less than zero."):
-            score.value = -2
 
-    def test_set_odd_value(self):
-        score = Score(10)
-        with pytest.raises(ValueError, match="The value of the scored points must be an even number."):
-            score.value = 7
+def test_set_negative_value():
+    score = Score(10)
+    with pytest.raises(ValueError, match="The value of the scored points must be not less than zero."):
+        score.value = -2
 
-    def test_set_decreasing_value(self):
-        score = Score(10)
-        with pytest.raises(ValueError, match="The value of the scored points should not decrease."):
-            score.value = 8
 
-    def test_diff_calculation(self):
-        score = Score(1)
-        score.value = 20000
-        assert score.diff == 19999
+def test_set_odd_value():
+    score = Score(10)
+    with pytest.raises(ValueError, match="The value of the scored points must be an even number."):
+        score.value = 15
 
-    def test_value_and_diff_after_multiple_changes(self):
-        score = Score(10)
-        score.value = 12
-        assert score.value == 12
-        assert score.diff == 2
-        score.value = 16
-        assert score.value == 16
-        assert score.diff == 4
+
+def test_set_decreasing_value():
+    score = Score(10)
+    with pytest.raises(ValueError, match="The value of the scored points should not decrease."):
+        score.value = 8
+
+
+def test_diff_calculation():
+    score = Score(1)
+    score.value = 20000
+    assert score.diff == 19999
+
+
+def test_value_and_diff_after_multiple_changes():
+    score = Score(10)
+    score.value = 12
+    assert score.value == 12
+    assert score.diff == 2
+    score.value = 16
+    assert score.value == 16
+    assert score.diff == 4
--- Code/Tests/test_core/test_tile.py
+++ Code/Tests/test_core/test_tile.py
@@ -3,21 +3,22 @@ import pytest
 from Core.game import Tile
 
 
-class TestTile:
+def test_initial_value():
+    tile = Tile(2)
+    assert tile.value == 2
 
-    def test_initial_value(self):
-        tile = Tile(2)
-        assert tile.value == 2
 
-    def test_value_setter(self):
-        tile = Tile(6)
-        tile.value = 10
-        assert tile.value == 10
+def test_value_setter():
+    tile = Tile(6)
+    tile.value = 10
+    assert tile.value == 10
 
-    def test_set_negative_value(self):
-        with pytest.raises(ValueError, match="The value of the tile must be not less than zero."):
-            Tile.validate_value(-2)
 
-    def test_set_odd_value(self):
-        with pytest.raises(ValueError, match="The value of the tile must be an even number."):
-            Tile.validate_value(7)
+def test_set_negative_value():
+    with pytest.raises(ValueError, match="The value of the tile must be not less than zero."):
+        Tile.validate_value(-2)
+
+
+def test_set_odd_value():
+    with pytest.raises(ValueError, match="The value of the tile must be an even number."):
+        Tile.validate_value(7)
